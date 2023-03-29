852b654d283178c8b4dba5d41a36154a4868eaa9
Abdesslam MEKOUAR <a.mekouar@outlook.com>
2023-03-29 10:16:45 -0700

    push 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,20 +7,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  int lengthD, lengthS;
+int lengthD, lengthS;
 
-  lengthD = 0;
-  lengthS = 0;
+lengthD = 0;
+lengthS = 0;
 
-  while (*(dest + lengthD) != '\0')
-    lengthD++;
+while (*(dest + lengthD) != '\0')
+lengthD++;
 
-  while (*(src + lengthS) != '\0' && lengthD < 97)
-    {
-      *(dest + lengthD) = *(src + lengthS);
-      lengthD++;
-      lengthS++;
-    }
-  *(dest + lengthD) = '\0';
-  return (dest);
+while (*(src + lengthS) != '\0' && lengthD < 97)
+{
+*(dest + lengthD) = *(src + lengthS);
+lengthD++;
+lengthS++;
+}
+*(dest + lengthD) = '\0';
+return (dest);
 }
