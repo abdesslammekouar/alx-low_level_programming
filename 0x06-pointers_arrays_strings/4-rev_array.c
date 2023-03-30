6e5deeb643867dfd422943a3034c5d878ecfdf46
Abdesslam MEKOUAR <a.mekouar@outlook.com>
2023-03-29 18:37:34 -0700

    push 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,11 +6,11 @@
  */
 void reverse_array(int *a, int n)
 {
-  int i, rev;
-  for (i = 0 ; i < n / 2 ; i++)
-    {
-      rev = a[i];
-      a[i] = a[n - i - 1];
-      a[n - i - 1] = rev;
-    }
+int i, rev;
+for (i = 0 ; i < n / 2 ; i++)
+{
+rev = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = rev;
+}
 }
