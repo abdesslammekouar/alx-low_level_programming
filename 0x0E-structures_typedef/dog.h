87ef7daa654c920b25c94db218d880e23e34ef8c
Abdesslam MEKOUAR <a.mekouar@outlook.com>
2023-04-17 16:56:32 -0700

    push dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+/**
+ * struct dog - list
+ * @name: pet name
+ * @age: pet age
+ * @owner: pet owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/**
+ * dog - rename strucut with typedef
+ */
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
