67ae84599f37ca8df84ccf2fe509112b2902ecbe
Abdesslam MEKOUAR <a.mekouar@outlook.com>
2023-04-18 18:04:18 -0700

    push update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,22 +10,27 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
+dog_t *p;
 
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
-		return (NULL);
-	p->name = malloc(strlen(name) + 1);
-	p->owner = malloc(strlen(owner) + 1);
-	if (p->name == NULL || p->owner == NULL)
-	{
-		free(name);
-		free(p);
-		free(owner);
-		return (NULL);
-	}
-	strcpy(p->name, name);
-	p->age = age;
-	strcpy(p->owner, owner);
-	return (p);
+p = malloc(sizeof(dog_t));
+if (p == NULL)
+return (NULL);
+p->name = malloc(strlen(name) + 1);
+p->owner = malloc(strlen(owner) + 1);
+if (p->name == NULL)
+{
+free(name);
+free(p);
+return (NULL);
+}
+if (p->owner == NULL)
+{
+free(p);
+free(owner);
+return (NULL);
+}
+strcpy(p->name, name);
+p->age = age;
+strcpy(p->owner, owner);
+return (p);
 }
